c1764d1fcef33d23d1c9c23171ec23f3e1afa0bf
GabrielStancu <gabriel.stancu07@yahoo.com>
2022-10-16 23:10:38 +0300

    Initialized the solution

---
+++ HacktoberfestConsole/HacktoberfestConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿var prStatus1 = "Pending...";
+var prStatus2 = "Pending...";
+var prStatus3 = "Pending...";
+var prStatus4 = "Pending...";
+
+Console.WriteLine($"PR1: {prStatus1}");
+Console.WriteLine($"PR2: {prStatus2}");
+Console.WriteLine($"PR3: {prStatus3}");
+Console.WriteLine($"PR4: {prStatus4}");
